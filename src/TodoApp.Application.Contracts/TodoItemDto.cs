b49ae125c9fd4a9b1ee222d3b7966fccd880e8ec
Yi Syuan <shelly10401@hotmail.com>
2024-03-05 09:39:17 +0800

    create

---
+++ src/TodoApp.Application.Contracts/TodoItemDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TodoApp
+{
+    public class TodoItemDto
+    {
+        public Guid Id { get; set; }
+        public string Text { get; set; }
+
+        public string Note { get; set; }
+    }
+}
